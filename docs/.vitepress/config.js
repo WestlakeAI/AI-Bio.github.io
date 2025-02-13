0fd8cd670c7d9f82bb60dbfbf66a79ef05a5159a
sqma <masiqi@westlake.edu.cn>
2025-02-14 04:26:52 +0800

    sync

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitepress';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: 'Westlake University',
-  base: '/AI-Bio/',
   description: 'CAIRI home page',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
